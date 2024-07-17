946003d45c8ac9c31e59a000a1a1e9cfd3e8d90e
Amit Mishra <mishraamit9129@gmail.com>
2024-07-18 00:14:19 +0530

    first commit

--- application.py
+++ application.py
@@ -2,14 +2,13 @@ import pickle
 from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
 
-## import ridge regressor model and standard Scaler pickle
-ridge_model = pickle.load(open('Models/3.2_ridge.pkl','rb'))
-standard_scaler = pickle.load(open('Models/3.2_scaler.pkl','rb'))
+## import model and standard Scaler pickle
+model = pickle.load(open('Models/modelForPrediction.pkl','rb'))
+scaler = pickle.load(open('Models/StandardScale.pkl','rb'))
 
 ## Route for home page
 @app.route('/')
@@ -18,21 +17,27 @@ def index():
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
+    result = ""
+
     if request.method=='POST':
-        Temperature = float(request.form.get('Temperature'))
-        RH = float(request.form.get('RH'))
-        Ws = float(request.form.get('Ws'))
-        Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC'))
-        DMC = float(request.form.get('DMC'))
-        ISI = float(request.form.get('ISI'))
-        Classes = float(request.form.get('Classes'))
-        Region = float(request.form.get('Region'))
+        Pregnancies = int(request.form.get('Pregnancies'))
+        Glucose = float(request.form.get('Glucose'))
+        BloodPressure = float(request.form.get('BloodPressure'))
+        SkinThickness = float(request.form.get('SkinThickness'))
+        Insulin = float(request.form.get('Insulin'))
+        BMI = float(request.form.get('BMI'))
+        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+        Age = float(request.form.get('Age'))
 
-        new_data_scaled = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
-        result = ridge_model.predict(new_data_scaled)
+        new_data_scaled = scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        predict = model.predict(new_data_scaled)
 
-        return render_template('home.html',result=result[0])
+        if predict[0] == 1:
+            result = 'Diabetic'
+        else:
+            result = 'Non-Diabetic'
+
+        return render_template('single_prediction.html',result=result)
 
     else:
         return render_template('home.html')
